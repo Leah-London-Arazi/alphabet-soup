5c5de99640a16bf2cc68f3b54c655d37d20c7b5a
danielezertau <danielezer@mail.tau.ac.il>
2024-08-25 21:35:24 +0300

    Add initial code for the character roulette black box attack

---
+++ character_roullete/character_roullete.py
@@ -0,0 +1,49 @@
+"""
+
+CharacterRoulette
+========================================
+(Black-box Generation of Adversarial Text Sequences to Evade Deep Learning Classifiers)
+
+"""
+
+from textattack import Attack
+from textattack.constraints.overlap import LevenshteinEditDistance
+from textattack.search_methods import GreedyWordSwapWIR
+from textattack.transformations import (
+    CompositeTransformation,
+    WordSwapNeighboringCharacterSwap,
+    WordSwapRandomCharacterDeletion,
+    WordSwapRandomCharacterInsertion,
+    WordSwapRandomCharacterSubstitution,
+)
+
+from textattack.goal_functions import ClassificationGoalFunction
+
+
+class IncreaseConfidence(ClassificationGoalFunction):
+    def __init__(self, *args, eps=0.2, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.eps = eps
+
+    def _get_score(self, model_output, _):
+        return model_output.max()
+
+    def _is_goal_complete(self, model_output, _):
+        return 1 - model_output.max() <= self.eps
+
+
+def character_roulette_black_box(model):
+    transformation = CompositeTransformation(
+        [
+            WordSwapNeighboringCharacterSwap(),
+            WordSwapRandomCharacterSubstitution(),
+            WordSwapRandomCharacterDeletion(),
+            WordSwapRandomCharacterInsertion(),
+        ]
+    )
+
+    constraints = [LevenshteinEditDistance(30)]
+    goal_function = IncreaseConfidence(model)
+    search_method = GreedyWordSwapWIR()
+
+    return Attack(goal_function, constraints, transformation, search_method)
