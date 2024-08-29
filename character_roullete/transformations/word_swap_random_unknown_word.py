be1fb61022d0d81ed7b34bd33b00ae03fa2131a2
danielezertau <danielezer@mail.tau.ac.il>
2024-08-29 18:07:33 +0300

    Implement transformation that swaps a word with a random word

--- character_roullete/transformations/word_swap_random_unknown_word.py
+++ character_roullete/transformations/word_swap_random_unknown_word.py
@@ -8,33 +8,27 @@ from textattack.models.wrappers import ModelWrapper
 import random
 import string
 class WordSwapRandomUnknownWord(WordSwap):
-    def __init__(self, model_wrapper, min_length=3, max_length=10, **kwargs):
+    def __init__(self, model_wrapper, num_random_words=10, min_length=3, max_length=10, **kwargs):
         super().__init__(**kwargs)
         if not isinstance(model_wrapper, ModelWrapper):
             raise TypeError(f"Got invalid model wrapper type {type(model_wrapper)}")
-        self.model = model_wrapper.model
+        self.model_wrapper = model_wrapper
 
         self.min_length = min_length
         self.max_length = max_length
+        self.num_random_words = num_random_words
 
     def _get_replacement_words(self, word):
         """Returns a list containing a random unknown word."""
         if len(word) <= 1:
             return []
 
-        candidate_words = []
+        candidate_words = [self._random_word() for _ in range(self.num_random_words)]
 
-        random_word = self._random_word()
-        while True:
-            tokenized_random_word = self.model.tokenize([random_word])
-            if tokenized_random_word == "hello":
-                pass
-        candidate_words.append(candidate_word)
-        
         return candidate_words
 
     def _random_word(self):
         length = random.randint(self.min_length, self.max_length)  # Random word length
         word = ''.join(random.choices(string.ascii_lowercase, k=length))  # Generate a word
         return word
-    
+
