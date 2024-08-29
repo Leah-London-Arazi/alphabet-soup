903a776b662b380e45ba0790a94dbe641ae1a4b5
danielezertau <danielezer@mail.tau.ac.il>
2024-08-29 18:15:38 +0300

    Use reimplemented search method in the main file

--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import textattack
 import transformers
 from character_roullete.transformations.word_swap_random_unknown_word import WordSwapRandomUnknownWord
 from character_roullete.character_roullete import IncreaseConfidence
+from character_roullete.search_methods.greedy_word_swap_threshold_wir import GreedyWordSwapThresholdWIR
 
 if __name__ == '__main__':
     # Load model, tokenizer, and model_wrapper
     model = transformers.AutoModelForSequenceClassification.from_pretrained("textattack/bert-base-uncased-imdb")
     tokenizer = transformers.AutoTokenizer.from_pretrained("textattack/bert-base-uncased-imdb")
     model_wrapper = textattack.models.wrappers.HuggingFaceModelWrapper(model, tokenizer)
-    
+
     # Construct our four components for `Attack`
-    from textattack.transformations import WordSwapEmbedding
-    from textattack.search_methods import GreedyWordSwapWIR
     goal_function = IncreaseConfidence(model_wrapper)
-    constraints =  []
+    constraints = []
     transformation = WordSwapRandomUnknownWord(model_wrapper)
-    search_method = GreedyWordSwapWIR()
+    search_method = GreedyWordSwapThresholdWIR(swap_threshold=0.05, debug=True)
     # Construct the actual attack
     attack = textattack.Attack(goal_function, constraints, transformation, search_method)
-    input_text = "I really enjoyed the new movie that came out last month."
-    label = 1 #Positive
+    input_text = "The cat sat on the mat."
+    label = 1  # Positive
     attack_result = attack.attack(input_text, label)
+    print(attack_result)
