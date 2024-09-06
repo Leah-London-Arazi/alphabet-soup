4e580583edaad15cd485af44bafbfacf0351fdd4
danielezertau <danielezer@mail.tau.ac.il>
2024-09-06 23:10:14 +0300

    Change epsilon and use a random sentence as input

--- unbounded_drift.py
+++ unbounded_drift.py
@@ -3,7 +3,7 @@ import textattack
 from search_methods.pez_gradient_search import PEZGradientSearch
 from transformations.nop import NOP
 from goal_functions.increase_confidence import IncreaseConfidence
-from utils.utils import get_model_wrapper, run_attack
+from utils.utils import get_model_wrapper, run_attack, random_sentence
 
 
 def initialize_prompt(token_embedding, prompt_len, device):
@@ -18,7 +18,7 @@ def unbounded_drift(model_name):
     model_wrapper = get_model_wrapper(model_name)
 
     # Construct our four components for `Attack`
-    goal_function = IncreaseConfidence(model_wrapper, query_budget=30, eps=0.1)
+    goal_function = IncreaseConfidence(model_wrapper, query_budget=30, eps=0.01)
     constraints = []
     transformation = NOP()
     search_method = PEZGradientSearch(model_wrapper, lr=0.1, wd=1, debug=True)
@@ -26,7 +26,7 @@ def unbounded_drift(model_name):
     # Construct the actual attack
     attack = textattack.Attack(goal_function, constraints, transformation, search_method)
 
-    run_attack(attack=attack, input_text="dknjks d,ddkls the alkdla.")
+    run_attack(attack=attack, input_text=random_sentence())
 
 if __name__ == '__main__':
     unbounded_drift("mnoukhov/gpt2-imdb-sentiment-classifier")
