383731a478b96b14fa2ac848c3e188a0176ba8df
danielezertau <danielezer@mail.tau.ac.il>
2024-09-06 23:09:51 +0300

    Implement random_sentence method and make random_word generate a word without spaces

--- utils/utils.py
+++ utils/utils.py
@@ -24,8 +24,13 @@ def run_attack(attack, input_text="The movie was filmed somewhere at some time."
 
 def random_word(min_len=3, max_len=10):
     length = random.randint(min_len, max_len)  # Random word length
-    word = ''.join(random.choices(string.printable, k=length))  # Generate a word
+    characters = string.digits + string.ascii_letters + string.punctuation
+    word = ''.join(random.choices(characters, k=length))  # Generate a word
     return word
+
+def random_sentence(min_len=3, max_len=10):
+    sen_length = random.randint(min_len, max_len)  # Random sentence length
+    return " ".join([random_word() for _ in range(sen_length)])
 
 def set_random_seed(seed=0):
     torch.manual_seed(seed + 0)
