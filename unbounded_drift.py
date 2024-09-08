468db85703c5295bf569ac38eada1a95a0779edd
leahlondon <leahtoprop@gmail.com>
2024-09-08 21:37:44 +0300

    pez attack adjustments

--- unbounded_drift.py
+++ unbounded_drift.py
@@ -2,8 +2,8 @@ import torch
 import textattack
 from search_methods.pez_gradient_search import PEZGradientSearch
 from transformations.nop import NOP
-from goal_functions.increase_confidence import IncreaseConfidence
-from utils.utils import get_model_wrapper, run_attack, random_sentence
+from goal_functions.increase_confidence import IncreaseConfidenceUntargeted, IncreaseConfidenceTargeted
+from utils.utils import get_model_wrapper, run_attack
 
 
 def initialize_prompt(token_embedding, prompt_len, device):
@@ -14,14 +14,17 @@ def initialize_prompt(token_embedding, prompt_len, device):
 
     return prompt_embeds
 
-def unbounded_drift(model_name):
+def unbounded_drift(model_name, targeted=False, max_iter=100):
     model_wrapper = get_model_wrapper(model_name)
 
     # Construct our four components for `Attack`
-    goal_function = IncreaseConfidence(model_wrapper, query_budget=30, eps=0.01)
+    if targeted:
+        goal_function = IncreaseConfidenceTargeted(model_wrapper, query_budget=max_iter)
+    else:
+        goal_function = IncreaseConfidenceUntargeted(model_wrapper, query_budget=max_iter)
     constraints = []
     transformation = NOP()
-    search_method = PEZGradientSearch(model_wrapper, lr=0.1, wd=1, debug=True)
+    search_method = PEZGradientSearch(model_wrapper, lr=0.4, wd=0, debug=True, max_iter=max_iter)
 
     # Construct the actual attack
     attack = textattack.Attack(goal_function, constraints, transformation, search_method)
@@ -30,3 +33,5 @@ def unbounded_drift(model_name):
 
 if __name__ == '__main__':
     unbounded_drift("mnoukhov/gpt2-imdb-sentiment-classifier")
+    unbounded_drift("finiteautomata/bertweet-base-sentiment-analysis", targeted=True, max_iter=100)
+    unbounded_drift("cardiffnlp/twitter-roberta-base-sentiment-latest")
