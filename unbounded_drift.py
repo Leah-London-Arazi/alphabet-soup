d7923b1c577b6a455c9249367710032f4ad8309e
danielezertau <danielezer@mail.tau.ac.il>
2024-09-19 20:25:50 +0300

    Remove the option for untargeted classification in unbounded drift

--- unbounded_drift.py
+++ unbounded_drift.py
@@ -2,7 +2,7 @@ import torch
 import textattack
 from search_methods.pez_gradient_search import PEZGradientSearch
 from transformations.nop import NOP
-from goal_functions.increase_confidence import IncreaseConfidenceUntargeted, IncreaseConfidenceTargeted
+from goal_functions.increase_confidence import IncreaseConfidenceTargeted
 from utils.utils import get_model_wrapper, run_attack
 
 
@@ -14,17 +14,15 @@ def initialize_prompt(token_embedding, prompt_len, device):
 
     return prompt_embeds
 
-def unbounded_drift(model_name, targeted=False, max_iter=100, filter_by_target_class=False, target_class=0):
+def unbounded_drift(model_name, max_iter=100, filter_by_target_class=False, target_class=0):
     model_wrapper = get_model_wrapper(model_name)
 
     # Construct our four components for `Attack`
-    if targeted:
-        goal_function = IncreaseConfidenceTargeted(model_wrapper, query_budget=max_iter, target_class=target_class)
-    else:
-        goal_function = IncreaseConfidenceUntargeted(model_wrapper, query_budget=max_iter)
+    goal_function = IncreaseConfidenceTargeted(model_wrapper, query_budget=max_iter, target_class=target_class)
     constraints = []
     transformation = NOP()
-    search_method = PEZGradientSearch(model_wrapper, target_class=target_class, lr=0.4, wd=0, debug=True, max_iter=max_iter, filter_by_target_class=filter_by_target_class)
+    search_method = PEZGradientSearch(model_wrapper, target_class=goal_function.target_class, lr=0.4, wd=0, debug=True,
+                                      max_iter=max_iter, filter_by_target_class=filter_by_target_class)
 
     # Construct the actual attack
     attack = textattack.Attack(goal_function, constraints, transformation, search_method)
@@ -33,5 +31,5 @@ def unbounded_drift(model_name, targeted=False, max_iter=100, filter_by_target_c
 
 if __name__ == '__main__':
     #unbounded_drift("mnoukhov/gpt2-imdb-sentiment-classifier")
-    unbounded_drift("finiteautomata/bertweet-base-sentiment-analysis", targeted=True, max_iter=5000, filter_by_target_class=True, target_class=2)
-    unbounded_drift("cardiffnlp/twitter-roberta-base-sentiment-latest", targeted=True, max_iter=5000, filter_by_target_class=True, target_class=2)
+    unbounded_drift("finiteautomata/bertweet-base-sentiment-analysis", max_iter=100, filter_by_target_class=True, target_class=2)
+    unbounded_drift("cardiffnlp/twitter-roberta-base-sentiment-latest", max_iter=100, filter_by_target_class=True, target_class=2)
