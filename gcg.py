407f237b6599c37cf13babe5d1d8f872091f8b59
danielezertau <danielezer@mail.tau.ac.il>
2024-09-19 20:23:18 +0300

    Move GCG to textattack

--- gcg.py
+++ gcg.py
@@ -1,63 +1,25 @@
-# implemented GCG algorithm from https://arxiv.org/pdf/2307.15043
-
-import torch
-import transformers
-from textattack.shared.utils import device as ta_device
-import utils.utils
-from textattack.models.wrappers import HuggingFaceModelWrapper
-
-def sample_control(control_toks, grad, topk=256):
-    top_indices = (-grad).topk(topk, dim=1).indices
-    new_token_pos = torch.randint(low=0, high=len(control_toks), size=(1,)).item()
-    new_token_idx = torch.randint(0, topk, size=(1,)).item()
-    new_token_val = top_indices[new_token_pos][new_token_idx]
-    new_control_toks = control_toks.clone()
-    new_control_toks[new_token_pos] = new_token_val
-    return new_control_toks
+import textattack
+from textattack.search_methods import BeamSearch
+from transformations.gcg_random_token_swap import GCGRandomTokenSwap
+from goal_functions.increase_confidence import IncreaseConfidenceTargeted
+from utils.utils import get_model_wrapper, run_attack
 
 
-def gcg_step(model, tokenizer, input_ids, top_k, target_label, retries=100):
-    curr_score = torch.nn.functional.softmax(model(input_ids=input_ids).logits, dim=1)[0][target_label]
-    grad = utils.utils.get_grad_wrt_func(HuggingFaceModelWrapper(model, tokenizer), input_ids, target_label)['gradient']
-    grad = grad / grad.norm(dim=-1, keepdim=True)
-    for i in range(retries):
-        # check if the replacement is better from the original
-        new_input_ids = sample_control(input_ids.squeeze(0), grad, top_k)
-        new_score = torch.nn.functional.softmax(model(input_ids=new_input_ids.unsqueeze(0)).logits, dim=1)[0][target_label]
-        if new_score > curr_score:
-            return new_input_ids
+def gcg(model_name, target_class=0, max_iter=100, max_retries_per_iter=100, top_k=256):
+    model_wrapper = get_model_wrapper(model_name)
 
-    raise Exception("max retries reached")
+    goal_function = IncreaseConfidenceTargeted(model_wrapper, query_budget=max_iter, target_class=target_class)
+    constraints = []
+    transformation = GCGRandomTokenSwap(model_wrapper, goal_function=goal_function, 
+                                        max_retries_per_iter=max_retries_per_iter, top_k=top_k)
+    # Greedy search
+    search_method = BeamSearch(beam_width=1)
 
+    # Construct the actual attack
+    attack = textattack.Attack(goal_function, constraints, transformation, search_method)
 
-def gcg(model, tokenizer, initial_text, top_k, num_iter, target_label, debug=False):
-    model.to(device=ta_device)
-    input_ids = tokenizer(initial_text,
-                          add_special_tokens=True,
-                          return_tensors="pt",
-                          padding=True,
-                          truncation=True).input_ids.to(device=ta_device)
-
-    for i in range(num_iter):
-        input_ids = gcg_step(model, tokenizer, input_ids, top_k, target_label)
-        input_ids = input_ids.unsqueeze(0)
-
-        score = torch.nn.functional.softmax(model(input_ids=input_ids).logits, dim=1)[0][target_label]
-        curr_text = tokenizer.decode(input_ids.squeeze(0))
-
-        if debug:
-            print(f"iteration: {i}")
-            print(f"curr_text: {curr_text}")
-            print(f"score: {score}")
-    return curr_text
-
-
-def main(model_name):
-    model = transformers.AutoModelForSequenceClassification.from_pretrained(model_name)
-    tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
-    initial_text = utils.utils.random_sentence()
-    return gcg(model, tokenizer, initial_text, top_k=256, num_iter=40, target_label=2, debug=True)
-
+    run_attack(attack=attack)
 
 if __name__ == '__main__':
-    main("cardiffnlp/twitter-roberta-base-sentiment-latest")
+    gcg("finiteautomata/bertweet-base-sentiment-analysis", target_class=0)
+    gcg("cardiffnlp/twitter-roberta-base-sentiment-latest", target_class=0)
