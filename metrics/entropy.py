0d8f4a6133574fe54384230aee33355def661644
danielezertau <danielezer@mail.tau.ac.il>
2024-09-26 19:40:10 +0300

    Implement character level entropy metric in textattack format

---
+++ metrics/entropy.py
@@ -0,0 +1,44 @@
+import torch
+from textattack.attack_results import FailedAttackResult, SkippedAttackResult
+from textattack.metrics import Metric
+
+
+class Entropy(Metric):
+    def __init__(self):
+        self.all_metrics = {}
+
+    def calculate(self, results):
+        entropy_results = []
+        for result in results:
+            if isinstance(result, FailedAttackResult):
+                continue
+            elif isinstance(result, SkippedAttackResult):
+                continue
+            else:
+                entropy_results.append(
+                    Entropy.char_level_entropy(result.perturbed_result.attacked_text.text)
+                )
+
+        self.all_metrics["avg_attack_entropy"] = round(
+            sum(entropy_results) / len(entropy_results), 2
+        )
+
+        return self.all_metrics
+
+
+    @staticmethod
+    def char_level_entropy(text):
+        # Create a set of unique characters
+        unique_chars = list(set(text))
+
+        # Convert characters to indices
+        indices = torch.tensor(list(range(len(unique_chars))), dtype=torch.long)
+
+        # Count character occurrences
+        num_chars = len(text)
+        counts = torch.bincount(indices, minlength=len(unique_chars))
+
+        # Convert counts to probabilities
+        probabilities = counts.float() / num_chars
+
+        return torch.sum(torch.special.entr(probabilities)).item()
