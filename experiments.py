3ba925c15b618d416e26391082aacb756fa38f59
danielezertau <danielezer@mail.tau.ac.il>
2024-09-28 22:04:05 +0300

    Change model_name to model_names to allow running the same experiment using multiple models

--- experiments.py
+++ experiments.py
@@ -1,9 +1,9 @@
 from textattack.metrics import Perplexity, AttackQueries, AttackSuccessRate
+from omegaconf import OmegaConf
 from utils.utils import set_random_seed, random_sentence
 from consts import ATTACK_NAME_TO_RECIPE, ATTACK_NAME_TO_PARAMS, AttackName
 from metrics.entropy import Entropy
 from utils.attack import run_attack
-from omegaconf import OmegaConf
 
 def get_attack_recipe(args):
     attack_name = AttackName(args.attack_name)
@@ -22,27 +22,31 @@ def get_attack_recipe(args):
 
 def run_single_experiment(args, metrics):
     attack_results = []
-    metrics_results = []
-    expr_times = []
-    for _ in range(args.num_repetitions):
-        attack_recipe = get_attack_recipe(args)
-        attack = attack_recipe.get_attack()
+    experiment_metrics = {}
+    for model_name in args.model_names:
+        per_model_time = []
+        per_model_metrics = []
+        args.model_name = model_name
+        for _ in range(args.num_repetitions):
+            attack_recipe = get_attack_recipe(args)
+            attack = attack_recipe.get_attack()
 
-        if args.rand_init_text:
-            init_text = random_sentence()
-        else:
-            init_text = args.initial_text
+            if args.rand_init_text:
+                init_text = random_sentence()
+            else:
+                init_text = args.initial_text
 
-        expr_result, expr_time = run_attack(attack=attack, input_text=init_text)
-        attack_results.append(expr_result)
-        expr_times.append(expr_time)
+            expr_result, expr_time = run_attack(attack=attack, input_text=init_text)
+            attack_results.append(expr_result)
+            per_model_time.append(expr_time)
 
-    for metric in metrics:
-        metrics_results.append(metric().calculate(attack_results))
+        for metric in metrics:
+            per_model_metrics.append(metric().calculate(attack_results))
 
-    metrics_results.append({"avg_attack_time_secs": round(sum(expr_times) / len(expr_times), 2)})
+        per_model_metrics.append({"avg_attack_time_secs": round(sum(per_model_time) / len(per_model_time), 2)})
 
-    return attack_results, metrics_results
+        experiment_metrics[model_name] = per_model_metrics
+    return experiment_metrics
 
 
 def run_experiments(metrics):
@@ -50,8 +54,9 @@ def run_experiments(metrics):
     config = OmegaConf.load("config.yaml")
     for experiment_config in config.experiments:
         experiment_args = OmegaConf.merge(config.defaults, experiment_config)
-        _, metric_results = run_single_experiment(experiment_args, metrics)
-        print(f"Metrics for {experiment_args.attack_name}: {metric_results}")
+        experiment_results = run_single_experiment(experiment_args, metrics)
+        for model_name, experiment_metrics in experiment_results.items():
+            print(f"Metrics for {experiment_args.attack_name} on {model_name}: {experiment_metrics}")
 
 if __name__ == '__main__':
     run_experiments([Entropy, Perplexity, AttackQueries, AttackSuccessRate])
