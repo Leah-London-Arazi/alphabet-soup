82c638f1da1203fdb06434f588206d38dde72013
leahlondon <leahtoprop@gmail.com>
2024-10-01 13:19:38 +0300

    removed log level from the config to a cmd arg + logging changes

--- experiments.py
+++ experiments.py
@@ -1,34 +1,24 @@
 import argparse
 
-from utils.utils import set_random_seed, random_sentence, disable_warnings, init_logger, get_root_logger
+from utils.utils import set_random_seed, random_sentence, disable_warnings, init_logger
 
 disable_warnings()
+set_random_seed()
 
 from tqdm import trange
 from textattack.metrics import Perplexity, AttackQueries, AttackSuccessRate
 from omegaconf import OmegaConf
-from consts import ATTACK_NAME_TO_RECIPE, ATTACK_NAME_TO_PARAMS, AttackName
 from metrics.entropy import Entropy
 from metrics.time import Time
 from utils.attack import run_attack
+from utils.recipes import get_attack_recipe_from_args
+from utils.utils import get_logger
 
 
-def get_attack_recipe(args):
-    attack_name = AttackName(args.attack_name)
-    attack_recipe_cls = ATTACK_NAME_TO_RECIPE[attack_name]
-    attack_params_cls = ATTACK_NAME_TO_PARAMS[attack_name]
-    attack_params = attack_params_cls(**args.attack_params)
-
-    attack_recipe = attack_recipe_cls(model_name=args.model_name,
-                                      targeted=args.targeted,
-                                      target_class=args.target_class,
-                                      confidence_threshold=args.confidence_threshold,
-                                      query_budget=args.query_budget,
-                                      attack_params=attack_params)
-    return attack_recipe
+logger = get_logger(__name__)
 
 
-def log_metrics(results, metrics, args, experiment_number, logger):
+def log_metrics(results, metrics, extra=None):
     metrics_results = []
     for metric in metrics:
         try:
@@ -36,57 +26,59 @@ def log_metrics(results, metrics, args, experiment_number, logger):
             if metric_result:
                 metrics_results.append(metric_result)
         except Exception as e:
-            logger.error(f"Caught exception while calculating "
-                         f"metrics in {args.attack_name} on {args.model_name}: {e}")
+            logger.error(f"Caught exception while calculating metrics: {e}", extra=extra)
             continue
 
-    logger.info(f"Metric results for experiment number {experiment_number}: "
-                f"attack {args.attack_name} on {args.model_name}: {metrics_results}")
+    logger.info(f"Metric results where written to file: {metrics_results}", extra=extra)
 
 
-def run_single_experiment(args, metrics, experiment_number):
-    logger = get_root_logger()
-    logger.info(f"Experiment {experiment_number} configuration: {args}")
+def run_single_experiment(experiment_num, experiment_args, metrics):
+    extra = dict(experiment_num=experiment_num, experiment_args=experiment_args)
+    logger.info(f"Running experiment", extra=extra)
+
+    attack_recipe = get_attack_recipe_from_args(experiment_args, from_command_line=False)
+
     expr_results = []
-    logger.info(f"Running experiment number {experiment_number}: "
-                f"attack {args.attack_name} on {args.model_name} "
-                f"for {args.num_repetitions} repetitions")
-    for rep_i in trange(args.num_repetitions):
-        logger.debug(f"Experiment {experiment_number} repetition {rep_i}")
-        attack_recipe = get_attack_recipe(args)
-        attack = attack_recipe.get_attack()
 
-        if args.rand_init_text:
+    for i in trange(experiment_args.num_repetitions):
+        attack = attack_recipe.get_attack()
+        print(i)
+
+        if experiment_args.rand_init_text:
             init_text = random_sentence()
         else:
-            init_text = args.initial_text
+            init_text = experiment_args.initial_text
         try:
             expr_rep_result = run_attack(attack=attack, input_text=init_text)
         except Exception as e:
-            logger.error(f"Caught exception while running "
-                         f"attack {args.attack_name} on {args.model_name}: {e}")
+            logger.error(f"Caught exception while running experiment: {e}")
             continue
+
         expr_results.append(expr_rep_result)
 
-    log_metrics(expr_results, metrics, args, experiment_number, logger)
+    log_metrics(results=expr_results, metrics=metrics, extra=extra)
+
 
 def run_experiments(metrics, config_file):
-    set_random_seed()
     config = OmegaConf.load(config_file)
-    init_logger(level_name=config.defaults.log_level)
+
     for experiment_num, experiment_config in enumerate(config.experiments):
         experiment_args = OmegaConf.merge(config.defaults, experiment_config)
         for model_name in experiment_args.model_names:
             for target_class in experiment_args.target_classes:
                 experiment_args.model_name = model_name
                 experiment_args.target_class = target_class
-                run_single_experiment(experiment_args, metrics, experiment_num)
+                run_single_experiment(experiment_num=experiment_num,
+                                      experiment_args=experiment_args,
+                                      metrics=metrics)
 
 
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config-file", type=str, required=True,
                         help="experiments configuration file")
+    parser.add_argument("--log-level", type=str, default="INFO",
+                        help="logging level")
     return parser
 
 
@@ -94,7 +86,10 @@ def main():
     parser = get_parser()
     args = parser.parse_args()
 
-    run_experiments([Entropy, Perplexity, AttackQueries, AttackSuccessRate, Time], args.config_file)
+    init_logger(level_name=args.log_level)
+
+    run_experiments(metrics=[Entropy, Perplexity, AttackQueries, AttackSuccessRate, Time],
+                    config_file=args.config_file)
 
 
 if __name__ == '__main__':
