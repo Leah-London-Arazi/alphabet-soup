d5d1bba7299844a2f57f3f0e53a8da64cf7fdb94
leahlondon <leahtoprop@gmail.com>
2024-10-14 21:29:11 +0300

    perplexity metric no exceptions

--- metrics/perplexity.py
+++ metrics/perplexity.py
@@ -22,17 +22,16 @@ class Perplexity(TextAttackPerplexity):
                     result.perturbed_result.attacked_text.text.lower()
                 )
 
-        ppl_orig = self.calc_ppl(self.original_candidates)
-        ppl_attack = self.calc_ppl(self.successful_candidates)
-        ppl_skipped = self.calc_ppl(self.skipped_candidates_ppl)
-
-        if len(ppl_orig) > 0:
+        if len(self.original_candidates) > 0:
+            ppl_orig = self.calc_ppl(self.original_candidates)
             self.all_metrics["avg_original_perplexity"] = round(ppl_orig, 2)
 
-        if len(ppl_attack) > 0:
+        if len(self.successful_candidates) > 0:
+            ppl_attack = self.calc_ppl(self.successful_candidates)
             self.all_metrics["avg_attack_perplexity"] = round(ppl_attack, 2)
 
-        if len(ppl_skipped) > 0:
+        if len(self.skipped_candidates_ppl) > 0:
+            ppl_skipped = self.calc_ppl(self.skipped_candidates_ppl)
             self.all_metrics["avg_skipped_perplexity"] = round(ppl_skipped, 2)
 
         return self.all_metrics
