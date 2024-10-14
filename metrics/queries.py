35c4f47b3778918165f8d1c25878cc84e518efc8
danielezertau <danielezer@mail.tau.ac.il>
2024-10-14 14:48:06 +0300

    Remove skipped attack queries reporting

--- metrics/queries.py
+++ metrics/queries.py
@@ -14,17 +14,11 @@ class Queries(Metric):
         for result in results:
             if isinstance(result, SuccessfulAttackResult):
                 succeeded_attack_queries.append(result.num_queries)
-            if isinstance(result, SkippedAttackResult):
-                skipped_attack_queries.append(result.num_queries)
-            if isinstance(result, FailedAttackResult):
+            if isinstance(result, (SkippedAttackResult, FailedAttackResult)):
                 continue
 
         if len(succeeded_attack_queries) > 0:
             self.all_metrics["avg_succeeded_attack_queries"] = round(
                 sum(succeeded_attack_queries) / len(succeeded_attack_queries), 2)
-
-        if len(skipped_attack_queries) > 0:
-            self.all_metrics["avg_skipped_attack_queries"] = round(
-                sum(skipped_attack_queries) / len(skipped_attack_queries), 2)
 
         return self.all_metrics
